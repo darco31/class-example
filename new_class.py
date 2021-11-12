45a25495231a4bfcc56036ff076fbefc92dfcee8
Stephen <sdarcy29@hotmail.com>
2021-11-12 15:20:45 +0000

    Initial commit

--- new_class.py
+++ new_class.py
@@ -2,3 +2,9 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
+john = Person("John", 23)
+mariam = Person("Mariam", 18)
+
+print(john.name + " is " + str(john.age) + " years old!!!")
