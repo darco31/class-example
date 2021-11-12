81b135ce93de6682f244cebf51df84e9a56f36e3
Stephen <sdarcy29@hotmail.com>
2021-11-12 16:45:46 +0000

    Add behaviours to the class and run program

--- new_class.py
+++ new_class.py
@@ -3,8 +3,18 @@ class Person:
         self.name = name
         self.age = age
 
+    def walk(self):
+        print(self.name + " is walking")
+
+    def speak(self):
+        print(
+            f"Hello my name is {self.name} and I am {str(self.age)} years old")
+
 
 john = Person("John", 23)
 mariam = Person("Mariam", 18)
 
-print(john.name + " is " + str(john.age) + " years old!!!")
+print(f"{john.name} {str(john.age)}")
+john.walk()
+john.speak()
+mariam.walk()
